4daaef68b76518c1a6d23327b32e17ce5acb0b4a
ElectroPanxo <juanmanuelrubialesdelacruz@gmail.com>
2022-02-03 12:33:32 +0100

    Add iconType to renderMenuIcon

--- views/dao/menu.php
+++ views/dao/menu.php
@@ -1,14 +1,18 @@
 <?php
 global $translator;
 
-function renderMenuIcon($iconClass, $textKey){
+function renderMenuIcon($iconClass, $textKey, $iconType){
     global $translator;
     $textVal = $translator->trans($textKey);
-    return "<span class='fas $iconClass marginIcon' aria-hidden='true'></span> <span>$textVal</span>";
+    $iconTypStr = 'fa';
+    if(isset($iconType)){
+        $iconTypStr = $iconType;
+    }
+    return "<span class='$iconTypStr $iconClass marginIcon' aria-hidden='true'></span> <span>$textVal</span>";
 };
 
-function addMenuItem($menu, $iconClass, $textKey, $submenuOrUrl){
-    $menu->addRight(renderMenuIcon($iconClass, $textKey), $submenuOrUrl);
+function addMenuItem($menu, $iconClass, $iconType, $textKey, $submenuOrUrl){
+    $menu->addRight(renderMenuIcon($iconClass, $textKey, isset($iconType) ? $iconType : null), $submenuOrUrl);
 }
 
 function renderMenuEntry($textKey, $to){
@@ -22,21 +26,21 @@ if ($USER->instructor) {
     $menu = new \Tsugi\UI\MenuSet();
     $menu->setHome('Code Test', 'index.php');
     if ('student-home.php' != basename($_SERVER['PHP_SELF'])) {
-        addMenuItem($menu, 'fa-user-graduate', 'navbarmenu.student.view', 'student-home.php');
-        addMenuItem($menu, 'fa-clipboard-check', 'navbarmenu.grade', 'grade.php');
+        addMenuItem($menu, 'fa-user-graduate', null, 'navbarmenu.student.view', 'student-home.php');
+        addMenuItem($menu, 'fa-clipboard-check', null, 'navbarmenu.grade', 'grade.php');
 
         $resultsSubmenu = [
             renderMenuEntry("navbarmenu.results.by.student", "results-student.php"),
             renderMenuEntry("navbarmenu.results.by.exercise", "results-exercise.php"),
             renderMenuEntry("navbarmenu.results.download", "results-download.php")
         ];
-        addMenuItem($menu, 'fa-poll-h', 'navbarmenu.results', $resultsSubmenu);
+        addMenuItem($menu, 'fa-poll-h', null, 'navbarmenu.results', $resultsSubmenu);
 
         $usagesSubmenu = [
             renderMenuEntry("navbarmenu.usage.by.student", "usage-student.php"),
             renderMenuEntry("navbarmenu.usage.by.exercise", "usage-exercise.php"),
         ];
-        addMenuItem($menu, 'fa-comments', 'navbarmenu.usages', $usagesSubmenu);
+        addMenuItem($menu, 'fa-comments', null, 'navbarmenu.usages', $usagesSubmenu);
         
         $buildSubmenu = [
             renderMenuEntry("navbarmenu.exercises.create", 'create-exercise.php'),
@@ -44,8 +48,8 @@ if ($USER->instructor) {
             renderMenuEntry("navbarmenu.exercises.authorkit", 'ak-projects-list.php'),
             renderMenuEntry("navbarmenu.exercises.codetest", 'exercises-management.php'),
         ];
-        addMenuItem($menu, 'fa-edit', 'navbarmenu.build', $buildSubmenu);
+        addMenuItem($menu, 'fa-file-code-o', null, 'navbarmenu.build', $buildSubmenu);
     } else {
-        addMenuItem($menu, 'fa-sign-out-alt', 'navbarmenu.exit.student.view', 'instructor-home.php');
+        addMenuItem($menu, 'fa-sign-out-alt', null, 'navbarmenu.exit.student.view', 'instructor-home.php');
     }
 }
